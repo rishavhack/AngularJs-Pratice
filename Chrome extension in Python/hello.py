282b4778f469140459ac628faedaff5182799e6e
Rishav <rishav.jnit@gmail.com>
2018-12-27 21:20:30 +0530

    Chrome extension in Python

---
+++ Chrome extension in Python/hello.py
@@ -0,0 +1,16 @@
+# Example Python script 
+# (for rapydscript, a python to javascript compiler)
+
+#def doHelloMessage():
+#    alert('hello')
+#doHelloMessage()
+
+# modify html page
+document.getElementById("result").innerHTML = 'Compiled Python script in Chrome' 
+
+console.log(window.location.href)
+# write into log 
+console.log('hello from python')
+console.log('hello from python')
+import nltk
+ 
