264f543dfdd9814fa18abda83b45e966f60c7e1a
Rishav <rishav.jnit@gmail.com>
2019-05-26 17:58:32 +0530

    Google SignIn

---
+++ Google SignIn/src/app/app.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+declare const gapi: any;
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'google-login';
+  public auth2: any;
+  private scope = [
+    'profile',
+    'email',
+    'https://www.googleapis.com/auth/plus.me',
+    'https://www.googleapis.com/auth/contacts.readonly',
+    'https://www.googleapis.com/auth/admin.directory.user.readonly'
+  ].join(' ');
+
+  // tslint:disable-next-line:use-life-cycle-interface
+  ngAfterViewInit() {
+    this.googleInit();
+  }
+
+  public googleInit() {
+    gapi.load('auth2', () => {
+      this.auth2 = gapi.auth2.init({
+        client_id: '586976473792-g03c38ntkob4ag7efq6c6d7hfhlcpqd1.googleusercontent.com',
+        cookiepolicy: 'single_host_origin',
+        scope: this.scope
+      });
+      this.attachSignin(document.getElementById('googleBtn'));
+    });
+  }
+  public attachSignin(element) {
+    this.auth2.attachClickHandler(element, {},
+      (googleUser) => {
+
+        const profile = googleUser.getBasicProfile();
+        console.log('Token || ' + googleUser.getAuthResponse().id_token);
+        console.log('ID: ' + profile.getId());
+        console.log('Name: ' + profile.getName());
+        console.log('Image URL: ' + profile.getImageUrl());
+        console.log('Email: ' + profile.getEmail());
+        // YOUR CODE HERE
+      }, (error) => {
+        alert(JSON.stringify(error, undefined, 2));
+      });
+  }
+
+
+}
